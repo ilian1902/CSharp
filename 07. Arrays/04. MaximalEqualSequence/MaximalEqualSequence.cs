e16cd4f05836b96c5f01c03c3f1494ead7bd1501
BoyanTodorov <boyan.digital@gmail.com>
2013-11-22 11:44:07 +0200

    C# Programming
    C# Programming

---
+++ 07. Arrays/04. MaximalEqualSequence/MaximalEqualSequence.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Write a program that finds the maximal sequence of equal elements in an array.
+ * Example: {2, 1, 1, 2, 3, 3, 2, 2, 2, 1}  {2, 2, 2}.
+ */
+
+using System;
+class MaximalEqualSequence
+{
+    static void Main()
+    {
+        int[] array = { 2, 1, 1, 2, 3, 3, 2, 2, 2, 1 };
+
+        int sequence = 0;
+        int maxSequence = 0;
+        int element = 0;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == array[i + 1])
+            {
+                sequence++;
+            }
+            else
+            {
+                sequence = 1;
+            }
+
+            if (sequence > maxSequence)
+            {
+                maxSequence = sequence;
+                element = array[i];
+            }
+        }
+
+        //Print Result
+        for (int i = 1; i <= maxSequence; i++)
+        {
+
+            if (i != maxSequence)
+            {
+                Console.Write("{0},", element);
+            }
+            else
+            {
+                Console.WriteLine("{0}", element);
+            }
+        }
+
+    }
+}
---
+++ 07. Arrays/05. MaxIncreasingSequence/MaxIncreasingSequence.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Write a program that finds the maximal increasing sequence in an array.
+ */
+
+using System;
+class MaxIncreasingSequence
+{
+    static void Main()
+    {
+        int[] array = { 3, 2, 3, 4, 2, 2, 4 };
+
+        int sequence = 0;
+        int maxSequence = 0;
+        int lastElement = 0;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] + 1 == (array[i + 1]))
+            {
+                sequence++;
+                lastElement = array[i] + 1;
+            }
+            else
+            {
+                sequence = 1;
+            }
+
+            if (sequence > maxSequence)
+            {
+                maxSequence = sequence;
+            }
+        }
+
+        //Print Result
+        int firstElement = lastElement - maxSequence + 1;
+        for (int i = 1; i <= maxSequence; i++, firstElement++)
+        {
+
+            if (i != maxSequence)
+            {
+                Console.Write("{0},", firstElement);
+            }
+            else
+            {
+                Console.WriteLine("{0}", firstElement);
+            }
+        }
+    }
+}
